fb33277a43e3a54230b7b875505e9ea893ea9ad3
NAMAN JAIN <naman.049259@tmu.ac.in>
2026-05-01 14:12:26 +0530

    catch added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import BlogLayout from './components/BlogLayout';
 import BlogList from './pages/BlogList';
 import BlogPost from './pages/BlogPost';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<BlogLayout />}>
           <Route index element={<BlogList />} />
           <Route path="post/:id" element={<BlogPost />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
