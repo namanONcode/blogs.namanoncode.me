1f9599c843c0d6caff575e5bac4d3ee46556292e
NAMAN JAIN <naman.049259@tmu.ac.in>
2026-05-01 14:56:16 +0530

    deb1

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,14 @@ import BlogPost from './pages/BlogPost';
 
 const App: React.FC = () => {
   return (
-    <>
-      <div style={{ position: 'fixed', top: '10px', right: '10px', color: '#f89820', fontSize: '10px', zIndex: 9999, fontFamily: 'monospace' }}>APP_MOUNTED_V3</div>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<BlogLayout />}>
-            <Route index element={<BlogList />} />
-            <Route path="post/:id" element={<BlogPost />} />
-          </Route>
-        </Routes>
-      </HashRouter>
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<BlogLayout />}>
+          <Route index element={<BlogList />} />
+          <Route path="post/:id" element={<BlogPost />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   );
 };
 
